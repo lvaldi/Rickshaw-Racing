de4f380c88808bdc8d1140b3789ef7ef53c76151
Jacob Brunette <brunette.jacob18@gmail.com>
2017-10-19 17:55:56 -0700

    added materials for crosshair script

---
+++ CrosshairScript/CrosshairScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairScript : MonoBehaviour {
+
+	private Rect position;
+	public Texture2D crosshairTexture;
+	public Rigidbody bulletRigidbody;
+
+	// Use this for initialization
+	void Start () {
+		position = new Rect(
+			(Input.mousePosition.x - crosshairTexture.width/2), 
+			(Screen.height - Input.mousePosition.y - crosshairTexture.height/2), 
+			crosshairTexture.width, 
+			crosshairTexture.height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		position = new Rect(
+			(Input.mousePosition.x - crosshairTexture.width/2), 
+			(Screen.height - Input.mousePosition.y - crosshairTexture.height/2), 
+			crosshairTexture.width, 
+			crosshairTexture.height);
+		if (Input.GetMouseButtonDown (0)) {
+			OnClick ();
+		}
+	}
+
+	void OnGUI() {
+		GUI.DrawTexture (position, crosshairTexture);
+	}
+
+	void OnClick(){
+		RaycastHit hit;
+		int surfaceLayer = 1 << 8;
+		Vector3 crosshairLocation = Input.mousePosition;
+		// This gives pixel coordinates, not in-world coordinates
+		// need to try using Camera.main.ScreenPointToRay instead
+		Ray ray = Camera.main.ScreenPointToRay(crosshairLocation);
+		Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+
+		if(Physics.Raycast(ray, out hit, Mathf.Infinity, surfaceLayer)) {
+			Rigidbody bulletClone = (Rigidbody)Instantiate (bulletRigidbody, hit.point, Random.rotation);
+		}
+		print ("screenPosition.x: " + position.x);
+		print ("screenPosition.y: " + position.y);
+	}
+}

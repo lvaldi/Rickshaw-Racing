53f21319e970a0386c2b85d6f980eb3829efd87c
Richard Le <richardle1997@gmail.com>
2017-10-19 17:34:32 -0700

    Add files via upload
    Added Player Script and Followcam

---
+++ Followcam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Followcam : MonoBehaviour {
+
+    public Transform player;
+    public Transform camTarget;
+    public Vector3 offset;
+
+    public float tightness = 0.5f;
+
+
+    // Use this for initialization
+    void Start() {
+        offset = camTarget.position - player.position;
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        transform.position = Vector3.Slerp(transform.position, player.position + offset, Time.deltaTime * 7.0f);
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    Rigidbody rb;
+    Player playerScript;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        playerScript = GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleControls();
+
+        
+	}
+
+    void HandleControls()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(transform.right * 5.0f * Time.deltaTime);
+        }
+    }
+}

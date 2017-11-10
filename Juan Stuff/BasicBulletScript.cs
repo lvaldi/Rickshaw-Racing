b0c8a0863d137047edb7c06001245e9e203723de
JuanLeee <yosohs@gmail.com>
2017-11-09 16:39:06 -0800

    Add files via upload

---
+++ Juan Stuff/BasicBulletScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicBulletScript : MonoBehaviour {
+    public GameObject emmitterObject;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter (Collision collision) {
+        GameObject emmitter = (GameObject)Instantiate(emmitterObject, transform.position, transform.rotation);
+        
+		Destroy (gameObject);
+    }
+}
---
+++ Juan Stuff/EmitterParticle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitterParticle : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        // Invisible particle emitter
+        ParticleSystem myParticles = GetComponent<ParticleSystem>();
+
+        myParticles.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        ParticleSystem myParticles = GetComponent<ParticleSystem>();
+        // Finish particle emit, destory object
+        if (!myParticles.isEmitting)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
